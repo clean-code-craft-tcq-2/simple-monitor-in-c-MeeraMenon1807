2398585c28c5217f75c238a1eaad0d5d5ee4e74c
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2022-02-14 20:30:39 +0530

    Update BMS.c

--- BMS.c
+++ BMS.c
@@ -15,7 +15,7 @@ bool checkSocOutOfRange(float soc) {
 }
 
 bool checkChargeRateOutOfRange(float chargeRate) {
-    return chargeRate > 0.8;
+    return chargeRate > 0.8F;
 }
 
 void printWarning(char* message, bool hasWarning) {
@@ -25,24 +25,41 @@ void printWarning(char* message, bool hasWarning) {
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    printWarning("Temperature out of range!", checkTemperatureOutOfRange(temperature));
-    printWarning("State of Charge out of range!", checkSocOutOfRange(soc));
-    printWarning("Charge Rate out of range!", checkChargeRateOutOfRange(chargeRate));
+    bool isTemperatureOutOfRange = checkTemperatureOutOfRange(temperature);
+    printWarning("Temperature out of range!", isTemperatureOutOfRange);
+    
+    bool isSocOutOfRange = checkSocOutOfRange(soc);
+    printWarning("State of Charge out of range!", isSocOutOfRange);
+    
+    bool isChargeRateOutOfRange = checkChargeRateOutOfRange(chargeRate);
+    printWarning("Charge Rate out of range!", isChargeRateOutOfRange);
+    
+    return isTemperatureOutOfRange || isSocOutOfRange || isChargeRateOutOfRange ? 0 : 1;
 }
 
 int main() {
+  assert(!checkOutOfRange(10, 20, 15.5F));
+  assert(checkOutOfRange(10, 20, 25.5F));
+  assert(checkOutOfRange(10, 20, 5.5F));
+  assert(!checkOutOfRange(10, 20, 10));
+  assert(!checkOutOfRange(10, 20, 20));
+  
+  assert(!checkTemperatureOutOfRange(25));
+  assert(checkTemperatureOutOfRange(46));
+  assert(checkTemperatureOutOfRange(-1));
+  assert(!checkTemperatureOutOfRange(0));
+  assert(!checkTemperatureOutOfRange(45));
+  
+  assert(!checkSocOutOfRange(70));
+  assert(checkSocOutOfRange(19));
+  assert(checkSocOutOfRange(81));
+  assert(!checkSocOutOfRange(20));
+  assert(!checkSocOutOfRange(80));
+  
+  assert(!checkChargeRateOutOfRange(0.7F));
+  assert(checkChargeRateOutOfRange(1));
+  assert(!checkChargeRateOutOfRange(0.8F));
+  
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
